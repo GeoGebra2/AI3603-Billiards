f431f9f7dbc90b1fda6836d1f5af3c6b073d3674
LFWANG <f7339208@gmail.com>
2025-12-27 16:42:34 +0800

    修改了被调用智能体位置，使之符合我们的代码

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,3 @@
 from .agent import Agent
-from .basic_agent import BasicAgent
+from .basic_agent import BasicAgent, NewAgent, MCTSAgent
 from .basic_agent_pro import BasicAgentPro
-from .new_agent import NewAgent
